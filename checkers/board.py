e9a2b0d3f051e43f046fb5e3ee3388f80bb85b71
Dmitriy Guk <dima.guk2004@gmail.com>
2023-04-25 17:18:35 +0300

    Fixed some bugs

--- checkers/board.py
+++ checkers/board.py
@@ -7,10 +7,79 @@ class Board:
         self.board = []
         self.white_left = self.black_left = 12
         self.white_kings = self.black_kings = 0
-        self.temp = None
-        self.triple_jump = None
-        self.counter = 0
-        self.create_board()
+        self.__create_board()
+
+    def __create_board(self):
+        """Creates the board"""
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                if col % 2 == ((row + 1) % 2):
+                    if row < 3:
+                        self.board[row].append(Piece(row, col, BLACK))
+                    elif row > 4:
+                        self.board[row].append(Piece(row, col, WHITE))
+                    else:
+                        self.board[row].append(0)
+                else:
+                    self.board[row].append(0)
+
+    def __traverse(self, start, stop, step, color, pos, side, skipped=None):
+        """Checks valid moves on the diagonals, side = False -> Left, side = True -> Right"""
+        if skipped is None:
+            skipped = []
+        moves = {}
+        last = []
+        for r in range(start, stop, step):
+            if not side and pos < 0 or side and pos >= COLS:
+                break
+
+            current = self.board[r][pos]
+            if current == 0:
+                if skipped and not last:
+                    break
+                elif skipped:
+                    moves[(r, pos)] = last + skipped
+                else:
+                    moves[(r, pos)] = last
+
+                if last:
+                    if step == -1:
+                        row = max(r - 3, 0)
+                    else:
+                        row = min(r + 3, ROWS)
+                    if color == WHITE and start == 3:
+                        if not side:
+                            moves.update(
+                                self.__traverse(r + step, row - 2, step, color, pos + 1, True, skipped=last))
+                            moves.update(
+                                self.__traverse(r + step, row - 2, step, color, pos - 1, False, skipped=last))
+                        else:
+                            moves.update(
+                                self.__traverse(r + step, row - 2, step, color, pos - 1, False, skipped=last))
+                            moves.update(
+                                self.__traverse(r + step, row - 2, step, color, pos + 1, True, skipped=last))
+                    else:
+                        if not side:
+                            moves.update(self.__traverse(r + step, row, step, color, pos + 1, True, skipped=last))
+                            moves.update(self.__traverse(r + step, row, step, color, pos - 1, False, skipped=last))
+                        else:
+                            moves.update(
+                                self.__traverse(r + step, row, step, color, pos - 1, False, skipped=last))
+                            moves.update(
+                                self.__traverse(r + step, row, step, color, pos + 1, True, skipped=last))
+                break
+            elif current.color == color:
+                break
+            else:
+                last = [current]
+
+            if not side:
+                pos -= 1
+            else:
+                pos += 1
+
+        return moves
 
     def evaluate(self):
         """Returns possible position for AI"""
@@ -30,6 +99,18 @@ class Board:
         if type(piece) != int:
             self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][
                 piece.col]
+            if abs(piece.row - row) == 6:
+                print(piece.row, piece.col, ":", row, col)
+                if abs(piece.row - row) == 6:
+                    if row - piece.row > 0:
+                        r_row = piece.row + 1
+                    else:
+                        r_row = piece.row - 1
+                    if col - piece.col > 0:
+                        r_col = piece.col + 1
+                    else:
+                        r_col = piece.col - 1
+                    self.remove([self.get_piece(r_row, r_col)])
             piece.move(row, col)
 
             if row == ROWS - 1 or row == 0:
@@ -45,21 +126,6 @@ class Board:
     def get_piece(self, row, col):
         """Returns piece in given position"""
         return self.board[row][col]
-
-    def create_board(self):
-        """Creates the board"""
-        for row in range(ROWS):
-            self.board.append([])
-            for col in range(COLS):
-                if col % 2 == ((row + 1) % 2):
-                    if row < 3:
-                        self.board[row].append(Piece(row, col, BLACK))
-                    elif row > 4:
-                        self.board[row].append(Piece(row, col, WHITE))
-                    else:
-                        self.board[row].append(0)
-                else:
-                    self.board[row].append(0)
 
     def remove(self, pieces):
         """Removes pieces"""
@@ -87,83 +153,14 @@ class Board:
         row = piece.row
 
         if piece.color == WHITE or piece.king:
-            moves.update(self._traverse(row - 1, max(row - 3, -1), -1, piece.color, left, False))
-            moves.update(self._traverse(row - 1, max(row - 3, -1), -1, piece.color, right, True))
+            moves.update(self.__traverse(row - 1, max(row - 3, -1), -1, piece.color, left, False))
+            moves.update(self.__traverse(row - 1, max(row - 3, -1), -1, piece.color, right, True))
         if piece.color == BLACK or piece.king:
-            moves.update(self._traverse(row + 1, min(row + 3, ROWS), 1, piece.color, left, False))
-            moves.update(self._traverse(row + 1, min(row + 3, ROWS), 1, piece.color, right, True))
-        self.counter = 0
-
-        return moves
-
-    def _traverse(self, start, stop, step, color, pos, side, skipped=None):
-        """Checks valid moves on the diagonals, side = False -> Left, side = True -> Right"""
-        if skipped is None:
-            skipped = []
-        moves = {}
-        last = []
-        for r in range(start, stop, step):
-            if not side and pos < 0 or side and pos >= COLS:
-                break
-
-            current = self.board[r][pos]
-            if current == 0:
-                if skipped and not last:
-                    break
-                elif skipped:
-                    moves[(r, pos)] = last + skipped
-                else:
-                    moves[(r, pos)] = last
-
-                if last:
-                    if step == -1:
-                        row = max(r - 3, 0)
-                    else:
-                        row = min(r + 3, ROWS)
-                    if color == WHITE and start == 3:
-                        self.counter += 1
-                        self.temp = moves
-                        if self.triple_jump is None:
-                            self.triple_jump = last
-                        if not side:
-                            moves.update(
-                                self._traverse(r + step, row - 2, step, color, pos + 1, True, skipped=last))
-                            moves.update(
-                                self._traverse(r + step, row - 2, step, color, pos - 1, False, skipped=last))
-                        else:
-                            moves.update(
-                                self._traverse(r + step, row - 2, step, color, pos - 1, False, skipped=last))
-                            moves.update(
-                                self._traverse(r + step, row - 2, step, color, pos + 1, True, skipped=last))
-                    else:
-                        self.counter += 1
-                        self.temp = moves
-                        if self.triple_jump is None:
-                            self.triple_jump = last
-                        if not side:
-                            moves.update(self._traverse(r + step, row, step, color, pos + 1, True, skipped=last))
-                            moves.update(self._traverse(r + step, row, step, color, pos - 1, False, skipped=last))
-                        else:
-                            moves.update(
-                                self._traverse(r + step, row, step, color, pos - 1, False, skipped=last))
-                            moves.update(
-                                self._traverse(r + step, row, step, color, pos + 1, True, skipped=last))
-                break
-            elif current.color == color:
-                break
-            else:
-                last = [current]
-
-            if not side:
-                pos -= 1
-            else:
-                pos += 1
-
-        if self.temp == moves and self.counter == 3:
-            moves[(stop - 1, pos)] = last + skipped + self.triple_jump
-            self.counter = 0
+            moves.update(self.__traverse(row + 1, min(row + 3, ROWS), 1, piece.color, left, False))
+            moves.update(self.__traverse(row + 1, min(row + 3, ROWS), 1, piece.color, right, True))
 
         return moves
 
     def get_board(self):
+        """Returns current state of game board"""
         return self.board
--- checkers/game.py
+++ checkers/game.py
@@ -13,19 +13,44 @@ class Game:
         self.second_player = True
         self.turn = None
         self.valid_moves = None
-        self._init()
+        self.__init()
         self.filename = None
 
-    def _init(self):
+    def __init(self):
         self.selected = None
         self.board = Board()
         self.turn = WHITE
         self.valid_moves = {}
 
+    def __move(self, row, col, sound):
+        """Moves pieces on the game board"""
+        piece = self.board.get_piece(row, col)
+        if self.selected and piece == 0 and (row, col) in self.valid_moves:
+            self.board.move(self.selected, row, col, sound)
+            skipped = self.valid_moves[(row, col)]
+            if skipped:
+                self.board.remove(skipped)
+            self.__change_turn()
+            self.selected = None
+        else:
+            return False
+
+        return True
+
+    def __change_turn(self):
+        """Change turn of the game"""
+        self.valid_moves = {}
+        if self.turn == WHITE:
+            self.turn = BLACK
+        else:
+            self.turn = WHITE
+
     def reset(self):
-        self._init()
+        """Restarts the game"""
+        self.__init()
 
     def save(self):
+        """Saves current game"""
         self.filename = "Save_" + str(len(os.listdir(r".\Saved_games")))
         os.mkdir(fr".\saved_games\{self.filename}")
         numpy.save(fr".\saved_games\{self.filename}\{self.filename}.npy", self.board.board, allow_pickle=True)
@@ -36,6 +61,7 @@ class Game:
                                                 self.turn[0]))
 
     def load_game(self, page, x, y):
+        """Load selected game"""
         saved_game = self.get_saved_matrix(page)[x][y]
         print(saved_game)
         if len(os.listdir(r".\saved_games")) > 0:
@@ -55,8 +81,9 @@ class Game:
                 self.turn = BLACK
 
     def select(self, row, col):
+        """Calculates selected piece"""
         if self.selected:
-            result = self._move(row, col, True)
+            result = self.__move(row, col, True)
             if not result:
                 self.selected = None
                 self.select(row, col)
@@ -69,61 +96,51 @@ class Game:
 
         return False
 
-    def _move(self, row, col, sound):
-        piece = self.board.get_piece(row, col)
-        if self.selected and piece == 0 and (row, col) in self.valid_moves:
-            self.board.move(self.selected, row, col, sound)
-            skipped = self.valid_moves[(row, col)]
-            if skipped:
-                self.board.remove(skipped)
-            self.change_turn()
-            self.selected = None
-        else:
-            return False
-
-        return True
-
     def winner(self):
+        """Returns winner of the game"""
         if self.board is not None:
             return self.board.winner()
 
-    def change_turn(self):
-        self.valid_moves = {}
-        if self.turn == WHITE:
-            self.turn = BLACK
-        else:
-            self.turn = WHITE
-
     def get_board(self):
+        """Returns the game board"""
         return self.board
 
     def get_selected(self):
+        """Returns selected piece"""
         return self.selected
 
     def get_turn(self):
+        """Returns current turn"""
         return self.turn
 
     def get_valid_moves(self):
+        """Returns valid moves of the game"""
         return self.valid_moves
 
     def get_filename(self):
+        """Returns filename to save/load"""
         return self.filename
 
     def get_board_to_draw(self):
+        """Returns the board for GUI"""
         return self.board.get_board()
 
     def ai_move(self, board):
+        """Makes a move if selected mode with AI"""
         PIECE_MOVE.play()
         self.board = board
-        self.change_turn()
+        self.__change_turn()
 
     def two_players(self):
+        """Sets game mode to 'Two players'"""
         self.second_player = True
 
     def against_ai(self):
+        """Sets game mode to 'Against AI'"""
         self.second_player = False
 
     def get_saved_matrix(self, page):
+        """Returns available saves on the page"""
         row, col, save_arr = 0, 0, [[]]
         for i in self.get_saves()[page]:
             save_arr[row].append(i)
@@ -136,6 +153,7 @@ class Game:
 
     @staticmethod
     def get_saves():
+        """Returns all available saves"""
         length, pos, arr = 0, 0, [[]]
         for i in os.listdir(r"./Saved_games"):
             if length <= 20:
